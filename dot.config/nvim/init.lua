6433bdcafc4b4afaabb896c8bd04c15005e3ba14
Alex Feinberg <alex@strlen.net>
2023-05-08 23:21:39 -0700

    Clean up vim config, add fzf plugin.

--- dot.config/nvim/init.lua
+++ dot.config/nvim/init.lua
@@ -3,35 +3,34 @@
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
--- Official plugin to make configuring the built-in LSP easier
-Plug 'neovim/nvim-lspconfig'
--- Plugin that makes configuring rust-analyzer easier
-Plug 'simrat39/rust-tools.nvim'
+	-- Official plugin to make configuring the built-in LSP easier
+	Plug 'neovim/nvim-lspconfig'
+	-- Plugin that makes configuring rust-analyzer easier
+	Plug 'simrat39/rust-tools.nvim'
 
---Plug 'kyazdani42/nvim-web-devicons'
-Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
-Plug 'junegunn/fzf.vim'
+	--Plug 'kyazdani42/nvim-web-devicons'
+	Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
+	Plug 'junegunn/fzf.vim'
 
-Plug 'overcache/NeoSolarized'
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-Plug 'chriskempson/base16-vim'
-Plug 'kyazdani42/nvim-web-devicons'
-Plug 'onsails/lspkind-nvim'
+	Plug 'overcache/NeoSolarized'
+	Plug 'vim-airline/vim-airline'
+	Plug 'vim-airline/vim-airline-themes'
+	Plug 'chriskempson/base16-vim'
+	Plug 'kyazdani42/nvim-web-devicons'
+	Plug 'onsails/lspkind-nvim'
 vim.call('plug#end')
 
 -- Always show the error column so the editor doesn't bounce around
 -- when there are warnings/errors in the buffer
-vim.api.nvim_command('set autoindent')
-vim.api.nvim_command('set expandtab')
-vim.api.nvim_command('set signcolumn=yes')
-vim.api.nvim_command('set smarttab')
-vim.api.nvim_command('set shiftwidth=4')
-vim.api.nvim_command('set tabstop=4')
-vim.api.nvim_command('set background=dark')
-vim.api.nvim_command('set ruler')
-vim.api.nvim_command('set number')
-vim.api.nvim_command('set termguicolors')
+vim.opt.signcolumn = "yes"
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.background = dark
+vim.opt.ruler = true
+vim.opt.number = true
+vim.opt.termguicolors = true
+
 vim.api.nvim_command('colorscheme NeoSolarized')
 
 -- Set up the LSP using rust-analyzer
@@ -50,3 +49,5 @@ vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 vim.api.nvim_set_keymap('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR><CR>', opts)
 -- show documentation with shift-K
 vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+
+vim.api.nvim_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
--- dot.zshrc
+++ dot.zshrc
@@ -9,7 +9,7 @@ gitAddModified() {
 export ZSH="${HOME}/.oh-my-zsh"
 ZSH_THEME="robbyrussell"
 zstyle ':omz:update' mode reminder  # just remind me to update when it's time
-plugins=(git)
+plugins=(git fzf)
 test -e "${HOME}/.zshrc.local" && source "${HOME}/.zshrc.local"
 source $ZSH/oh-my-zsh.sh
 unalias gam
